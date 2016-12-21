7fc686260e3f7cfa281c71b480bcbf26560f7c96
Ubuntu <ubuntu@ip-172-31-45-70.us-west-2.compute.internal>
2016-12-21 05:09:29 +0000

    Added explorer for eth sidechain, needs refactor

---
+++ PExplore/app/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
+
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'mainCtrl'
+            }).
+            when('/block/:blockId', {
+                templateUrl: 'views/blockInfos.html',
+                controller: 'blockInfosCtrl'
+            }).
+            when('/transaction/:transactionId', {
+                templateUrl: 'views/transactionInfos.html',
+                controller: 'transactionInfosCtrl'
+            }).
+            when('/address/:addressId', {
+                templateUrl: 'views/addressInfo.html',
+                controller: 'addressInfoCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }])
+    .run(function($rootScope) {
+        var web3 = new Web3();
+        var eth_node_url = 'http://35.163.149.80:8545'; // TODO: remote URL
+//	var eth_node_url = 'http://ec2-54-238-157-117.ap-northeast-1.compute.amazonaws.com:8545';
+        web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
+        $rootScope.web3 = web3;
+        function sleepFor( sleepDuration ){
+            var now = new Date().getTime();
+            while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+        }
+        var connected = false;
+        if(!web3.isConnected()) {
+            $('#connectwarning').modal({keyboard:false,backdrop:'static'}) 
+            $('#connectwarning').modal('show') 
+        }
+    });
